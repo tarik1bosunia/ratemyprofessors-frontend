0306297da0f6c5d3b0e7aa4dbedeacb96468fb10
md tarik bosunia <bosuniamdtarik005@gmail.com>
2024-07-22 14:55:49 +0600

    refactoring on components/sections/home/JoinRMPFamilySection.tsx and remove constants

--- components/sections/home/JoinRMPFamilySection.tsx
+++ components/sections/home/JoinRMPFamilySection.tsx
@@ -1,40 +1,13 @@
+import {
+    JOIN_RMP_FAMILY_SECTION_TITLE,
+    JOIN_RMP_FAMILY_SECTION_SUBTITLE,
+    JOIN_RMP_FAMILY_SECTION_IMAGE_BOXES,
+    JOIN_RMP_FAMILY_SECTION_BG
+} from '@/constants'
 
-// const styles = <style>
-// .rmp_image_box{
-//     -webkit-box-align: center;
-//     align-items: center;
-//     display: flex;
-//     flex-direction: column;
-//     -webkit-box-pack: justify;
-//     justify-content: space-between;
-//     max-height: 320px;
-// }
-// .rmp_image{
-//     height: 226px;
-//     margin-bottom: 24px;
-//     max-width: 100%;
-//     width: 340px;
-// }
-// .rmp_title{
-//     font-size: 26px;
-//     line-height: 26px;
-//     margin: 0px auto;
-//     max-width: 300px;
-//     text-align: center;
-//     font-family: Poppins, sans-serif;
-//     font-weight: 700;
-// }
-// .homepage_pkgds_pink_bacground{
-//     left: calc(-575px + 50vw);
-//     position: absolute;
-//     top: 1020px;
-// }
-// .homepage_pkgds_blue_bacground{
-//     position: absolute;
-//     right: calc(-190px + 50vw);
-//     top: 300px;
-// }
-// </style>
+import Image from 'next/image'
+import styles from '@/components/sections/home/join_rmp_family.module.css'
+
 
 const JoinRMPFamilySection = () => {
   return (
@@ -42,28 +15,23 @@ const JoinRMPFamilySection = () => {
     <div className="rmp mb-[80px] relative z-40">
         <div className="my-0 mx-auto w-full max-w-[1280px]">
             <div className="mb-[88px] text-center">
-                <div className="font-black text-3xl mb-4">Join the RMP Family </div>
-                <div className="text-2xl">Love RMP? Let&apos;s make it official.</div>
+                <div className="font-black text-3xl mb-4">{JOIN_RMP_FAMILY_SECTION_TITLE} </div>
+                <div className="text-2xl">{JOIN_RMP_FAMILY_SECTION_SUBTITLE}</div>
             </div>
             <div className="flex flex-row mb-[72px] justify-around">
-                <div className="rmp_image_box">
-                    <img alt="Lady with a pencil" src="images/home_rmp_section/instructional-slide-pencil-lady.svg" className="rmp_image"/>
-                    <div className="rmp_title">Manage and edit your ratings</div>
-                </div>
-    
-                <div className="rmp_image_box">
-                    <img alt="Person making an anonymous entry" src="images/home_rmp_section/instructional-slide-mystery-lady.svg" className="rmp_image"/>
-                    <div className="rmp_title">Your ratings are always anonymous</div>
-                </div>
-                <div className="rmp_image_box">
-                    <img alt="Thumb War" src="images/home_rmp_section/instructional-slide-thumb-war.svg" className="rmp_image"/>
-                    <div className="rmp_title">Like or dislike ratings</div>
-                </div>
+                {
+                    JOIN_RMP_FAMILY_SECTION_IMAGE_BOXES.map((image_box, index)=>(
+                        <div key={index} className={styles.rmp_image_box}>
+                        <Image alt={image_box.alt} src={image_box.image} className={styles.rmp_image}/>
+                        <div className={styles.rmp_title}>{image_box.title}</div>
+                    </div>
+                    ))
+                }
             </div>
         </div>
     </div>
-    <img src="{% static 'images/home_rmp_section/pink_background.svg' %}" alt="" className="homepage_pkgds_pink_bacground"/>
-    <img src="{% static 'images/home_rmp_section/blue_background.svg' %}" alt="" className="homepage_pkgds_blue_bacground"/>
+    <Image src={JOIN_RMP_FAMILY_SECTION_BG.pink} alt="" className={styles.homepage_pkgds_pink_bacground}/>
+    <Image src={JOIN_RMP_FAMILY_SECTION_BG.blue} alt="" className={styles.homepage_pkgds_blue_bacground}/>
 
 </div>
   )
--- constants.ts
+++ constants.ts
@@ -38,3 +38,37 @@ export const SEARCH_PROFESSOR_PLACEHOLDER =  "Professor name"
 // change search view buttons texts
 export const SCHOOL_CHANGE_SEARCH_VIEW_BUTTON_TEXT = "I'd like to look up a professor by name"
 export const PROFESSOR_CHANGE_SEARCH_VIEW_BUTTON_TEXT = "I want to find a professor at a school"
+
+// join rmp family sections
+
+import LADY_WITH_PENCIL_IMAGE from '@/public/images/home_rmp_section/instructional-slide-pencil-lady.svg'
+import MYSTRY_LADY_IMAGE from '@/public/images/home_rmp_section/instructional-slide-mystery-lady.svg'
+import THUMB_WAR_IMAGE from '@/public/images/home_rmp_section/instructional-slide-thumb-war.svg'
+import RMP_PINK_BACKGROUND_IMAGE from '@/public/images/home_rmp_section/pink_background.svg'
+import RMP_BLUE_BACKGROUND_IMAGE from '@/public/images/home_rmp_section/blue_background.svg'
+
+export const JOIN_RMP_FAMILY_SECTION_TITLE = 'Join the RMP Family'
+export const JOIN_RMP_FAMILY_SECTION_SUBTITLE = "Love RMP? Let's make it official."
+
+export const JOIN_RMP_FAMILY_SECTION_BG ={
+    pink: RMP_PINK_BACKGROUND_IMAGE,
+    blue: RMP_BLUE_BACKGROUND_IMAGE,
+}
+
+export const JOIN_RMP_FAMILY_SECTION_IMAGE_BOXES = [
+    {
+        title: "Manage and edit your ratings",
+        image: LADY_WITH_PENCIL_IMAGE,
+        alt: 'Lady with a pencil',
+    },
+    {
+        title: "Your ratings are always anonymous",
+        image: MYSTRY_LADY_IMAGE,
+        alt: 'Person making an anonymous entry',
+    },
+    {
+        title: "Like or dislike ratings",
+        image: THUMB_WAR_IMAGE,
+        alt: 'Thumb War',
+    },
+]
